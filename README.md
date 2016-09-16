2d5b6abba95d6f67cb99805d50aeb56b212b32c7
Paul Pagnan <paul@pagnan.com.au>
2016-09-16 19:39:10 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Simple Chat API
+A simple chat API for the coderfactory blog article
---
+++ src/app.js
@@ -0,0 +1,43 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import io from 'socket.io';
+import config from '../config/config.json';
+import path from 'path';
+
+// setup server
+const app = express();
+const server = http.createServer(app);
+
+const socketIo = io(server);
+
+// Allow CORS
+app.use(cors());
+
+// Render a API index page
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
+
+// Start listening
+server.listen(process.env.PORT || config.port);
+console.log(`Started on port ${config.port}`);
+
+// Setup socket.io
+socketIo.on('connection', socket => {
+  const username = socket.handshake.query.username;
+  console.log(`${username} connected`);
+
+  socket.on('client:message', data => {
+    console.log(`${username}: ${data}`);
+
+    // message received from client, now broadcast it to everyone else
+    socket.broadcast.emit('server:message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${username} disconnected`);
+  });
+});
+
+export default app;
